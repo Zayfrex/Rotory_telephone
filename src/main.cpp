9d55e07421c96535098e1393ebf3e828387de9a6
Zayfrex <116486489+Zayfrex@users.noreply.github.com>
2026-04-14 19:49:29 +0200

    counting

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,40 @@
 Rotory Counter every number on the dial corrisponds to the number of high pulses >> ex. 5 = 5 high pulses*/
 
 #define SPEAKER_PIN 9
-#define ROTORY_PIN A5
+//#define ROTORY_PIN A5
+#define START_Button 2  
+#define ROTORY_PIN 3 
 
-void playMelody(int pin);
+bool start_flag = false;
+volatile bool start_stop_flag_reading = false; // deklare it as volatile, because i change it in the interrupt
 
-void setup() {
-  pinMode(SPEAKER_PIN, OUTPUT);
-  pinMode(ROTORY_PIN, INPUT);
-  Serial.begin(9600);
-  playMelody(SPEAKER_PIN);
-}
+bool number_flag = false;
+unsigned long timestamp_number_button = 0;
+
+uint8_t counter_number = 0;
+
+const int BOUNCING_FILTER = 100;
+
+
 uint16_t rotoryValue = 0;
 uint8_t volts = 0;
 
+void playMelody(int pin);
+void start_reading();
+void start_counting();
+
+void setup() {
+  Serial.begin(76800);
+  pinMode(SPEAKER_PIN, OUTPUT);
+  pinMode(ROTORY_PIN, INPUT);
+  pinMode(START_Button, INPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(START_Button), start_reading, CHANGE);
+  playMelody(SPEAKER_PIN);
+}
+
 void loop() {
-  rotoryValue = analogRead(ROTORY_PIN);
-  volts = rotoryValue * 5/1024;
-  Serial.println(volts);
+  start_counting();
 }
 
 void playMelody(int pin) {
@@ -31,5 +48,38 @@ void playMelody(int pin) {
   delay(durations[i]);
   noTone(pin);
   delay(50);
+  }
+}
+
+
+void start_reading()
+{
+  static unsigned long timestamp_start_reading = 0; // static variable
+
+  if (millis() - timestamp_start_reading > BOUNCING_FILTER)
+  {
+    timestamp_start_reading = millis();
+    start_stop_flag_reading = !start_stop_flag_reading;
+
+    Serial.println("Change State");
+  }
+}
+
+void start_counting()
+{
+  uint8_t pin_state = digitalRead(ROTORY_PIN);
+  if (pin_state == HIGH && number_flag == false)
+  {
+    if (millis() - timestamp_number_button > BOUNCING_FILTER)
+    {
+      timestamp_number_button = millis();
+      counter_number++;
+      Serial.println(counter_number);
+      number_flag = true;
+    }
+  }
+  else if (pin_state == LOW)
+  {
+    number_flag = false;
   }
 }
