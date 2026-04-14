646a27f4df55483b7746b6a20782bd46185f6506
Zayfrex <matteorizzuto2116@gmail.com>
2026-04-14 11:41:06 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,25 @@
 
 #define SPEAKER_PIN 9
 
+void playMelody(int pin);
 
 void setup() {
   pinMode(SPEAKER_PIN, OUTPUT);
 }
 
 void loop() {
-  analogWrite(SPEAKER_PIN, 128); // 50% duty cycle
+  playMelody(SPEAKER_PIN);
+  delay(2000);
+}
+
+void playMelody(int pin) {
+  int notes[] = {440, 494, 523, 587};
+  int durations[] = {200, 200, 200, 400};
+
+  for (int i = 0; i < 4; i++) {
+  tone(pin, notes[i]);
+  delay(durations[i]);
+  noTone(pin);
+  delay(50);
+  }
 }
