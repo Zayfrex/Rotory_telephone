900d64ff11fd030024071e75d4ef2067c88c034f
Zayfrex <116486489+Zayfrex@users.noreply.github.com>
2026-04-16 16:11:03 +0200

    as

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,10 @@ Switch for ISR pulled down to gnd
 #define START_Button 2  
 #define ROTORY_PIN 3 
 #define TONE_DURATION 500
+#define DIAL_TIMEOUT 300
 
 enum NOTES{
-  C,
+  C=1,
   D,
   E,
   F,
@@ -38,9 +39,12 @@ const int BOUNCING_FILTER = 100;
 uint16_t rotoryValue = 0;
 uint8_t volts = 0;
 
-void playMelody(int pin);
+void playMelody(int );
 void start_reading();
 void start_counting();
+void playNote(uint8_t,int);
+void nokiaringtone(int );
+void starwars(int );
 
 void setup() {
   Serial.begin(76800);
@@ -54,8 +58,16 @@ void setup() {
 
 void loop() {
   start_counting();
+
+  // Only read counter after dial has stopped pulsing after the DIAL_TIMEOUT
+  if(counter_number && (millis() - timestamp_number_button > DIAL_TIMEOUT)){
+    playNote(counter_number,SPEAKER_PIN);
+    delay(500);
+    counter_number = 0;
+  }
 }
 
+//Turn On Melody to test the phone
 void playMelody(int pin) {
   int notes[] = {440, 494, 523, 587};
   int durations[] = {200, 200, 200, 400};
@@ -68,6 +80,32 @@ void playMelody(int pin) {
   }
 }
 
+
+void nokiaringtone(int pin) {
+  int notes[]     = {659, 587, 370, 415, 554, 494, 294, 330, 494, 440, 277, 330, 440};
+  int durations[] = {125, 125, 250, 250, 125, 125, 250, 250, 125, 125, 250, 250, 500};
+
+  for (int i = 0; i < 13; i++) {
+    tone(pin, notes[i]);
+    delay(durations[i]);
+    noTone(pin);
+    delay(30);
+  }
+}
+
+void starwars(int pin) {
+  int notes[]     = {440, 440, 440, 349, 523, 440, 349, 523, 440, 659, 659, 659, 698, 523, 415, 349, 523, 440};
+  int durations[] = {300, 300, 300, 200, 100, 300, 200, 100, 500, 300, 300, 300, 200, 100, 300, 200, 100, 500};
+
+  for (int i = 0; i < 18; i++) {
+    tone(pin, notes[i]);
+    delay(durations[i]);
+    noTone(pin);
+    delay(30);
+  }
+}
+
+
 void playNote(uint8_t note,int pin){ //IF needed --------!!!!! need to add the counter reset 
   switch (note)
   {
@@ -76,7 +114,7 @@ void playNote(uint8_t note,int pin){ //IF needed --------!!!!! need to add the c
       break;
 
     case NOTES::D:
-      tone(pin, 294),TONE_DURATION;
+      tone(pin, 294,TONE_DURATION);
       break;
 
     case NOTES::E:
@@ -97,6 +135,14 @@ void playNote(uint8_t note,int pin){ //IF needed --------!!!!! need to add the c
 
     case NOTES::B:
       tone(pin, 494,TONE_DURATION);
+      break;
+
+    case 8:
+      nokiaringtone(pin);
+      break;
+
+    case 9:
+      starwars(pin);
       break;
 
     default:
@@ -120,6 +166,10 @@ void start_reading()
 
 void start_counting()
 {
+  // Only count pulses while start button is active
+  if (!start_stop_flag_reading) 
+    return;
+
   uint8_t pin_state = digitalRead(ROTORY_PIN);
   if (pin_state == HIGH && number_flag == false)
   {
@@ -135,4 +185,4 @@ void start_counting()
   {
     number_flag = false;
   }
-}
+} 
